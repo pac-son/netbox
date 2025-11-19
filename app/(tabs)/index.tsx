a8a8b88100039c28092e47db25f06aa2a102deb5
Peterson Amblessed <amblessedpetercns@gmail.com>
2025-11-19 21:21:56 +0100

    implemented the useFetch hook and added a flatlist to loopthrough the latest movies

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,19 @@
-import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView, ActivityIndicator, FlatList } from 'react-native';
 import React from 'react';
 import SearchBar from '@/components/SearchBar';
 import { useRouter } from 'expo-router';
+import useFetch from '@/services/useFetch';
+import { fetchMovies } from '@/services/api';
+import MovieCard from '@/components/MovieCard';
 
 const Index = () => {
   const router = useRouter();
+
+  const {
+    data: movies,
+    loading: Loading,
+    error: moviesError
+  } = useFetch(() => fetchMovies({ query: ''}))
 
   return (
     <View className="flex-1 bg-purple-950">
@@ -28,12 +37,39 @@ const Index = () => {
           resizeMode="contain"
         />
 
-        <View className='flex-roe mt-5'>
+        {Loading ?  (
+          <ActivityIndicator 
+            size='large'
+            color='#0000ff'
+            className='mt-10 self-center'
+          />
+        ) : moviesError ? (
+          <Text>Error: {moviesError?.message}</Text>
+        ) : (
+          <View className='flex-roe mt-5'>
           <SearchBar 
             onPress={() => router.push('/(tabs)/search')}
             placeholder='Search for movies'
           />
+
+          <>
+            <Text className='text-lg text-white font-bold mt-5 mb-3'>Latest Movies</Text>
+
+            <FlatList 
+              data={movies}
+              keyExtractor={(item) => item.id.toString()}
+              numColumns={3}
+              renderItem={({ item }) => (
+                <MovieCard {...item} />
+              )}
+              columnWrapperClassName='justify-start gap-5 pr-1.5 mb-2.5'
+              className='mt-2 pb-32'
+              scrollEnabled={false}
+            />
+          </>
         </View>
+        )}
+
       </ScrollView>
     </View>
   );
