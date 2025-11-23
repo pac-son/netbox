93e826a5533350851a66ea804dc74269ebe08b4c
Peterson Amblessed <amblessedpetercns@gmail.com>
2025-11-23 09:20:46 +0100

    setup type for props and pass the parameters appropriately

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -5,17 +5,19 @@ import { TextInput, View } from 'react-native';
 interface Props {
   placeholder: string;
   onPress?: () => void;
+  value?: string;
+  onChangeText?: (text: string) => void;
 }
 
-const SearchBar = ({ placeholder, onPress }: Props) => {
+const SearchBar = ({ placeholder, onPress, value, onChangeText }: Props) => {
   return (
     <View style={{ flexDirection: 'row', alignItems: 'center' }} className='bg-dark-200 rounded-full px-5 py-4'>
       <Ionicons name='search' size={22} />
       <TextInput 
         onPress={onPress}
         placeholder={placeholder}
-        value=''
-        onChangeText={() => {}}
+        value={value}
+        onChangeText={onChangeText}
         placeholderTextColor="#a8b5db"
         className='flex-1 ml-2 text-white text-base'
       />
